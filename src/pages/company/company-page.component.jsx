2754b0f2247a3932e24294ceec18885e946dfb55
Enot Hueda <enot.hueda@gmail.com>
2020-03-15 13:51:32 -0600

    adding operator to avoid forms if previous documents, has not created or selected

--- src/pages/company/company-page.component.jsx
+++ src/pages/company/company-page.component.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
 import CompanyProfile from '../../components/company-profile/company-profile.component';
 import AddCompany from '../../components/add-company/add-company.component';
+import { selectCurrentUser  } from '../../redux/user/user.selectors';
 
 import './company-page.styles.scss';
 
-const CompanyPage = () => (
+const CompanyPage = ({ currentUser }) => (
 	<div className='company-page'>
 		<h1>Company Profile</h1>
 		<CompanyProfile />
-		<AddCompany />		
+		{
+			currentUser ?
+			<AddCompany />	:
+			<h4>Please Sign In First</h4>
+		}
+			
 	</div>
 );
 
-export default CompanyPage;
+const mapStateToProps = state => ({
+	currentUser: selectCurrentUser(state)
+})
+
+export default connect(mapStateToProps)(CompanyPage);
--- src/pages/ranchs/ranchs.component.jsx
+++ src/pages/ranchs/ranchs.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import AddRanch from '../../components/add-ranch/add-ranch.component';
 import AddSectors from '../../components/add-sectors/add-sectors.component';
 import ActualRanch from '../../components/actual-ranch/actual-ranch.component';
 import RanchSelect from '../../components/ranch-select/ranch-select.component';
+import { selectCurrentCompany } from '../../redux/company/company.selectors';
+import { selectCurrentRanch }  from '../../redux/ranch/ranch.selectors';
 
 class Ranchs extends React.Component {
    
     render() {
+        const { currentCompany, currentRanch } = this.props;
         return(
             <div>
                 <h1>Ranch Setup</h1>                
@@ -20,13 +24,25 @@ class Ranchs extends React.Component {
                     <span> Sectors | </span>
                     <span> Settings </span>            
                 </div>
-                <AddRanch />
-                <AddSectors />                 
+                {
+                    currentCompany ?
+                    <AddRanch /> :
+                    <h4>Please Add or Select a Company</h4>
+                }
+                {
+                    currentRanch ?
+                    <AddSectors /> :
+                    <h4>Please Add or Select a Ranch</h4>
+                }
+                                 
             </div>
         )
     }
 }   
     
+const mapStateToProps = state => ({
+    currentCompany: selectCurrentCompany(state),
+    currentRanch: selectCurrentRanch(state)
+})
 
-
-export default Ranchs;
+export default connect(mapStateToProps)(Ranchs);
